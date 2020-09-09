652dbaca3f380826a6e857e638fd93d1941cab1a
brookeperkins <brookemmperkins@gmail.com>
2020-09-09 15:32:38 -0700

    venue test passed

--- __tests__/venues.test.js
+++ __tests__/venues.test.js
@@ -6,7 +6,22 @@ describe('Venue model', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
-  it('creates a venue with the insert method', () => {
+  it('creates a venue with the insert method', async() => {
+    const createdVenue = await Venue.insert({
+      venueName: 'Moda Center',
+      artistName: 'Neil Diamond',
+      scheduledTime: '03-24-20 at 7:00 PM'
+    });
+    const { rows } = await pool.query(
+      'SELECT * FROM venues WHERE id = $1', [createdVenue.id]);
 
+    expect(rows[0]).not.toBeUndefined();
+    expect(createdVenue).toEqual({
+      id: expect.any(String),
+      venueName: 'Moda Center',
+      artistName: 'Neil Diamond',
+      scheduledTime: '03-24-20 at 7:00 PM'
+    });
   });
+  
 });
